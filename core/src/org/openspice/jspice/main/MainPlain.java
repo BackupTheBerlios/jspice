4f0370738edf098c25632bca54ed407b891a2c62
sfkleach <sfkleach>
2004-07-26 22:00:41 +0000

    Improved error handling - more files checked in

---
+++ core/src/org/openspice/jspice/main/MainPlain.java
@@ -0,0 +1,16 @@
+package org.openspice.jspice.main;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: steve
+ * Date: Jul 22, 2004
+ * Time: 8:22:36 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MainPlain {
+
+    public static final void main( final String[] args ) {
+        new Main().perform( false, "" );
+    }
+
+}
